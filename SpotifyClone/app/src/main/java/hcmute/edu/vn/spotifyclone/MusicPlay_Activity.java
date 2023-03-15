f952e89bfb059b704139201d598b2d5bdd67b091
votranbaonguyen <20110138@student.hcmute.edu.vn>
2023-03-15 21:48:19 +0700

    First create project

---
+++ SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/MusicPlay_Activity.java
@@ -0,0 +1,14 @@
+package hcmute.edu.vn.spotifyclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicPlay_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_play);
+    }
+}
