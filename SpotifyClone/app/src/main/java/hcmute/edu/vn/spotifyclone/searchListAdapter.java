6c53742ad2ec81b746c7c22833aecae584ac2bc0
NNQA <anhla12h@gmail.com>
2023-03-19 11:20:36 +0700

    feat(quocanh_dev): create fake data with model(user), adapter(searchListAdapter)

--- SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/searchListAdapter.java
+++ SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/searchListAdapter.java
@@ -1,2 +1,56 @@
-package hcmute.edu.vn.spotifyclone;public class searchListAdapter {
+package hcmute.edu.vn.spotifyclone;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.List;
+
+public class searchListAdapter extends RecyclerView.Adapter<searchListAdapter.ViewHolder> {
+
+    private List<user> mluser;
+
+    public searchListAdapter(List<user> mluser) {
+        this.mluser = mluser;
+    }
+
+    @NonNull
+    @Override
+    public searchListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_search, parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull searchListAdapter.ViewHolder holder, int position) {
+        user user = mluser.get(position);
+        if(user == null) return;
+        holder.imageView.setImageResource(user.getImage());
+        holder.txtName.setText(user.getName());
+        holder.txtad.setText(user.getAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mluser != null) return mluser.size();
+        return 0;
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ShapeableImageView imageView;
+        TextView txtName, txtad;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageSearch);
+            txtName = itemView.findViewById(R.id.titleName);
+            txtad = itemView.findViewById(R.id.desciption);
+        }
+    }
 }
