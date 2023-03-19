1fa94d5439acd7ecdfa301b1fa27a91b88b0fc0a
votranbaonguyen <20110138@student.hcmute.edu.vn>
2023-03-19 19:06:47 +0700

    update UI for Home screen

--- SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/MainActivity.java
+++ SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/MainActivity.java
@@ -1,14 +1,31 @@
 package hcmute.edu.vn.spotifyclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
+
+
+        Home home_fragment = new Home();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().replace(R.id.main_fragment,home_fragment).commit();
     }
 }
