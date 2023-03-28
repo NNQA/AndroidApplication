886fb216fa524f3beaa7d2747d44fd5bab1813c5
votranbaonguyen <20110138@student.hcmute.edu.vn>
2023-03-28 15:21:51 +0700

    add login authentication

--- SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/AuthenticationActivity.java
+++ SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/AuthenticationActivity.java
@@ -1,14 +1,97 @@
 package hcmute.edu.vn.spotifyclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class AuthenticationActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    public EditText tiEmailInput;
+    public EditText tiPassword;
 
+    public Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        tiEmailInput = findViewById(R.id.tiemail);
+        tiPassword = findViewById(R.id.tipassword);
+
+        signIn = findViewById(R.id.SignInButton);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = tiEmailInput.getText().toString().trim();
+                String password = tiPassword.getText().toString().trim();
+
+                if(email.isEmpty() && password.isEmpty()){
+
+                    Toast.makeText(getApplicationContext(),"You need to input all field",Toast.LENGTH_LONG).show();
+                }else{
+                    signIn(email,password);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //reload();
+        }
+    }
+
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Login Success",
+                                    Toast.LENGTH_SHORT).show();
+//                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName("Nguyen Dep Giai").build();
+//                            user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                @Override
+//                                public void onComplete(@NonNull Task<Void> task) {
+//                                    if (task.isSuccessful()) {
+//                                        Log.d("TAG", "User profile updated.");
+//                                    }
+//                                }
+//                            });
+                            System.out.println(user.getDisplayName());
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
     }
 }
--- SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/MainActivity.java
+++ SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/MainActivity.java
@@ -6,15 +6,33 @@ import androidx.fragment.app.FragmentManager;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
 //    Fragment Initiating
     public Home home_fragment = new Home();
     public Profile profile_fragment = new Profile();
@@ -31,6 +49,22 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        //createAccount("herobaonguyen@gmail.com","123456");
+
+       db.collection("usersA").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+           @Override
+           public void onComplete(@NonNull Task<QuerySnapshot> task) {
+               if(task.isSuccessful()){
+                   for(QueryDocumentSnapshot document : task.getResult()){
+                       Log.d("tag", document.getId() + " => " + document.getData());
+                       Log.d("tag", document.getId() + " => " + document.getData());
+                   }
+               }else{
+                   Log.w("TAG", "Error getting documents.", task.getException());
+               }
+           }
+       });
 
         BottomNavigationView bottomNavigationView = new BottomNavigationView(this);
 
@@ -62,5 +96,55 @@ public class MainActivity extends AppCompatActivity {
         });
 
         fragmentManager.beginTransaction().replace(R.id.main_fragment,home_fragment).commit();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //reload();
+        }
+    }
+
+    private void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d("Register","CreateUserWithEmai:Success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                }else{
+                    Log.w("Register", "createUserWithEmail:failure", task.getException());
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Register Fail","CreateUserWithEmai:Success");
+            }
+        });
+    }
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
     }
 }
