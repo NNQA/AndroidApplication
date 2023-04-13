dd34e744d97cd0cc01ddc7c82850bd1247263823
votranbaonguyen <20110138@student.hcmute.edu.vn>
2023-04-13 21:17:47 +0700

    add register

---
+++ SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/Register.java
@@ -0,0 +1,98 @@
+package hcmute.edu.vn.spotifyclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class Register extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private Button signUp;
+    private EditText email;
+    private EditText name;
+    private EditText password;
+    private EditText rePassword;
+
+    private Button gobackLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signUp = findViewById(R.id.SignUpButton);
+        gobackLogin = findViewById(R.id.backtoLogin);
+
+        email = findViewById(R.id.etEmail);
+        name = findViewById(R.id.etName);
+        password = findViewById(R.id.etPassword);
+        rePassword = findViewById(R.id.etRePassword);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!email.getText().toString().isEmpty() &&
+                        !name.getText().toString().isEmpty() &&
+                        !password.getText().toString().isEmpty() &&
+                        !rePassword.getText().toString().isEmpty()
+                ){
+                    if(password.getText().toString().trim().equals(rePassword.getText().toString().trim())){
+                        register(email.getText().toString(),password.getText().toString(),name.getText().toString());
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Your retype password are not correct",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(),"You need to fill all the blank",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        gobackLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void register(String email, String password, String name){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Register", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName(name).build();
+                            user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(),"SignUp Success",Toast.LENGTH_SHORT).show();
+                                        onBackPressed();
+                                    }
+                                }
+                            });
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                        }
+                    }
+                });
+    }
+}
