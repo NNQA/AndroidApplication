55a672715e24f6fdebaf681d723e044ead07fde7
Bao Nguyen <votranbaonguyen220702@gmail.com>
2023-04-19 17:43:51 +0700

    Refactor some cod

--- SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/CarouselAdapter.java
+++ SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/CarouselAdapter.java
@@ -1,39 +1,60 @@
 package hcmute.edu.vn.spotifyclone;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 import hcmute.edu.vn.spotifyclone.model.Album;
+import hcmute.edu.vn.spotifyclone.model.Song;
 
 public class CarouselAdapter extends RecyclerView.Adapter<CarouselAdapter.ViewHolder> {
-    private List<Album> albumList;
+    private List<Song> albumList;
     private Context context;
 
-    public CarouselAdapter(List<Album> albumList, Context context) {
+    public CarouselAdapter(List<Song> albumList, Context context) {
         this.albumList = albumList;
         this.context = context;
     }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.album_item, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Album album = albumList.get(position);
-        holder.album_title.setText(album.getAlbumTitle());
-        holder.album_image.setImageResource(album.getAlbumImage());
+        Song song = albumList.get(position);
+        InputStream inputStream = null;
+        try {
+            inputStream = new URL(song.getImage()).openStream();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+        holder.song_image.setImageBitmap(bitmap);
+        holder.song_name.setText(song.getSongName());
+        holder.song_item_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateSongScreen(song.getSongId());
+            }
+        });
     }
 
     @Override
@@ -43,13 +64,20 @@ public class CarouselAdapter extends RecyclerView.Adapter<CarouselAdapter.ViewHo
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        public ImageView album_image;
-        public TextView album_title;
+        public ImageView song_image;
+        public TextView song_name;
+        public LinearLayout song_item_view;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            album_image = itemView.findViewById(R.id.album_image);
-            album_title = itemView.findViewById(R.id.album_title);
+            song_image = itemView.findViewById(R.id.song_image);
+            song_name = itemView.findViewById(R.id.song_name);
+            song_item_view = itemView.findViewById(R.id.song_item_view);
         }
+    }
+    void navigateSongScreen(String songId) {
+        Intent intent = new Intent(context,MusicPlay_Activity.class);
+        intent.putExtra("sondId",songId);
+        context.startActivity(intent);
     }
 }
