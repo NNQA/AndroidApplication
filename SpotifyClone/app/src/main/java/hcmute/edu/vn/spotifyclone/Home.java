999f8964f0acbb4c598de125f62bdfaf809adf3a
Bao Nguyen <votranbaonguyen220702@gmail.com>
2023-04-20 17:32:18 +0700

    add new layout for playlist

--- SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/Home.java
+++ SpotifyClone/app/src/main/java/hcmute/edu/vn/spotifyclone/Home.java
@@ -1,5 +1,6 @@
 package hcmute.edu.vn.spotifyclone;
 
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -16,7 +17,10 @@ import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -25,6 +29,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
+import hcmute.edu.vn.spotifyclone.model.Playlist;
 import hcmute.edu.vn.spotifyclone.model.Song;
 
 
@@ -50,11 +55,33 @@ public class Home extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         listTitle = view.findViewById(R.id.list_title);
         recyclerView = view.findViewById(R.id.recycler_view);
-
-        setupAllSongRecycleView();
+//        setupAllSongRecycleView();
+        new LoadDataAsyncTask().execute();
         return view;
     }
 
+    private class LoadDataAsyncTask extends AsyncTask<Void, Void, FirestoreRecyclerOptions<Song>> {
+
+        @Override
+        protected FirestoreRecyclerOptions<Song> doInBackground(Void... voids) {
+            // Heavy work goes here, e.g. loading data from Firebase or network
+            Query query = database.collection("songs").limit(20);
+            FirestoreRecyclerOptions<Song> option = new FirestoreRecyclerOptions.Builder<Song>()
+                    .setQuery(query,Song.class).build();
+            return option;
+        }
+
+        @Override
+        protected void onPostExecute(FirestoreRecyclerOptions<Song> option) {
+            super.onPostExecute(option);
+            // Update the UI on the main thread
+            adapter = new CarouselSongAdapter(option,getContext());
+            recyclerView.setAdapter(adapter);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+            recyclerView.setLayoutManager(layoutManager);
+            adapter.startListening();
+        }
+    }
 
     void setupAllSongRecycleView() {
          Query query = database.collection("songs").limit(20);
@@ -68,6 +95,8 @@ public class Home extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
 
     }
+
+
 
     @Override
     public void onStart() {
@@ -90,3 +119,4 @@ public class Home extends Fragment {
         adapter.notifyDataSetChanged();
     }
 }
+
